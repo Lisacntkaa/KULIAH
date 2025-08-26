04362c65dfc9281d3202a1e39e119553a4c527b4
Lisacntkaa <lisa.chantika03@gmail.com>
2025-08-26 11:39:32 +0900

    variabel

---
+++ Flutter/flutter_application/lib/bin/latihansatu.dart
@@ -0,0 +1,15 @@
+void main() {
+  String name = "lisa cantika";
+  int umur = 20;
+  print("Nama saya adalah $name dan umur saya $umur tahun");
+
+  double berat = 48.9;
+  num tinggi = 165;
+  print("Tinggi saya adalah $tinggi cm dan berat badan saya adalah $berat kg");
+
+  dynamic warna = "hitam";
+  print("Warna favorit saya adalah $warna");
+
+  bool isActive = true;
+  print("Apakah mahasiswa ini aktif : $isActive");
+}
